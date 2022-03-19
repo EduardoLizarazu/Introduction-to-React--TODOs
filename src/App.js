f4cfe98de497de7cc903e63e7e012435da9c795f
eduardolizarazu <jelo2021@gmail.com>
2022-03-19 11:35:21 -0400

    Components from ToDo App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
+import { TodoList } from "./TodoList";
+import { TodoSearch } from "./TodoSearch";
+// import './App.css';
+
+const toDos = [
+  { text: "cut onion", completed: false },
+  { text: "Take course of intro to React", completed: false },
+  { text: "Cry with the Crybaby", completed: false },
+]
 
 function App(props) {
+  // React necesita que solo enviemos una etiqueta, entoces creamos una etiqueta invisible para no llenarlo de div
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Edward
-        </a>
-        <p>
-          { props.greet }
-        </p>
-        <p>
-          { props.children }
-        </p>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        { toDos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        )) }
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
---
+++ src/TodoCounter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoCounter() {
+    return (
+        <h2>Has Completed 2 of 3 ToDos</h2>
+    );
+}
+
+export { TodoCounter };
