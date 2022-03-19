4546567754282a1920774e6dafee45a5d64080cc
eduardolizarazu <jelo2021@gmail.com>
2022-03-19 12:47:08 -0400

    event management

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,8 +2,17 @@ import React from "react";
 import './CreateTodoButton.css';
 
 function CreateTodoButton(props) {
+    // const onClickButton = () => alert("Modal!");
+    const onClickButton = (msg) => alert(msg);
+
     return (
-        <button className="CreateTodoButton">+</button>
+        <button 
+            className="CreateTodoButton"
+            // onClick={onClickButton}
+            onClick={() => onClickButton("With a Parameter")}
+        >
+            +
+        </button>
     );
 }
 
