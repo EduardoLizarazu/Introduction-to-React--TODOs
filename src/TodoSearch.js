3c0ac92e5eb8aaef5840f1f853b1aed0bdd8b36b
eduardolizarazu <jelo2021@gmail.com>
2022-03-20 16:47:45 -0400

    State management

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import './TodoSearch.css';
 
+// Work the State in the past
+// // Now use React Hooks
+
+// class Component extends React.Component {
+//     constructor () {
+//         this.state = {
+//             ducky : 'Edward'
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <div onClick={() => this.setState('Jose')}>{this.state.ducky}</div>
+//         );
+//     }
+// }
+
 function TodoSearch() {
+    // const [state, setState] = useState(initialState);
+    const [searchValue, setSearchValue] = React.useState('');
+    // [
+    //     'Juan',
+    //     setState()
+    // ]
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value)
     };
-    return (
+    return [
         <input 
             className="TodoSearch" 
-            placeholder="Write the new task"
-            onChange={onSearchValueChange}
-        />
-    );
+            placeholder="Write a new task"
+            value={ searchValue }
+            onChange={ onSearchValueChange }
+        />,
+        <p>{ searchValue }</p>
+    ];
 }
 export { TodoSearch };
